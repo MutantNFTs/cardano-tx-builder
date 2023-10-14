f14f5dadff3fe7d91ca096c9cf030a1367ce3410
Geraldo Neto <geraldo.lopes@outlook.com>
2023-10-15 00:50:30 +0100

    fix(encodeAssetMap): remove unnecessary fallback

--- src/encodeAssetMap.ts
+++ src/encodeAssetMap.ts
@@ -2,12 +2,12 @@ import { AssetMap, EncodedAssetMap } from "./types";
 
 export const encodeAssetMap = (assetMap: AssetMap): EncodedAssetMap => {
   const encodedMap = new Map<Buffer, Map<Buffer, bigint | number>>();
-  const sortedPolicies = [...Object.keys(assetMap || {})].sort();
+  const sortedPolicies = [...Object.keys(assetMap)].sort();
 
   for (const policyId of sortedPolicies) {
     const policyIdAssetsMap = new Map<Buffer, bigint | number>();
     const assets = assetMap[policyId];
-    const sortedAssets = [...Object.keys(assets || {})].sort();
+    const sortedAssets = [...Object.keys(assets)].sort();
 
     for (const assetName of sortedAssets) {
       policyIdAssetsMap.set(
